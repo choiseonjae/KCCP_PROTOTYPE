8e208e03543fd3557780190c37f90a5d97f42736
choiseonjae <homesking@naver.com>
2019-05-20 21:55:22 +0900

    스크랩 시 추가 삭제 바로 바로 최신화 수정할 것 : 1. ID 채팅 목록이 아닌 NAME으로 출력하기

--- app/src/main/java/com/example/androideatit/MyScrap.java
+++ app/src/main/java/com/example/androideatit/MyScrap.java
@@ -1,6 +1,7 @@
 package com.example.androideatit;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +11,8 @@ import android.util.Log;
 import com.example.androideatit.Adapter.RoomAdapter;
 import com.example.androideatit.Common.Common;
 import com.example.androideatit.Model.Board;
+import com.example.androideatit.Model.Favorite;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -43,54 +46,57 @@ public class MyScrap extends AppCompatActivity {
     }
 
     private void getData() {
+
         final DatabaseReference scrapRef = Common.getDatabase(Common.SCRAP).child(myID);
         final DatabaseReference roomRef = Common.getDatabase(Common.ROOM);
 
-        scrapRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        // scrap - id 까지 참조. key : boardId | value : location
+        scrapRef.addChildEventListener(new ChildEventListener() {
+                                           @Override
+                                           public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                                               final String boardID = dataSnapshot.getKey();
+                                               final Favorite loaction = dataSnapshot.getValue(Favorite.class);
+                                               roomRef.child(loaction.getLocation()).child(boardID).addListenerForSingleValueEvent(new ValueEventListener() {
+                                                   @Override
+                                                   public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                       Board board = dataSnapshot.getValue(Board.class);
+                                                       adapter.add(board, dataSnapshot.getKey());
+                                                       adapter.notifyDataSetChanged();
+                                                   }
 
-                // 내가 한 스크랩들
-                for (DataSnapshot scrap : dataSnapshot.getChildren()) {
-                    final String boardID = scrap.getKey(); // 게시글 ID
-                    Log.e("게시글 ID : ", boardID);
+                                                   @Override
+                                                   public void onCancelled(@NonNull DatabaseError databaseError) {
+                                                       Log.e("취소 에러 확인 : ", databaseError.toString());
 
-                    // 방 DB를 검색해옴. / 나중에는 검색이 아니라 scrap에 구를 저장 해 놓을거임
-                    roomRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                   }
+                                               });
 
-                            Log.e("방 전체 정보 : ", dataSnapshot.toString());
 
-                            for (DataSnapshot town : dataSnapshot.getChildren()) {
+                                           }
 
-                                Log.e("방 정보 : ", town.toString());
-                                Log.e("게시글과 글이 동일 : ", town.child(boardID).exists() + "");
+                                           @Override
+                                           public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                                               adapter.notifyDataSetChanged();
+                                           }
 
-                                // 해당 구에 스크랩 한 글이 존재
-                                if (town.child(boardID).exists()) {
-                                    Board board = town.child(boardID).getValue(Board.class);
-                                    adapter.add(board, boardID);
-                                    adapter.notifyDataSetChanged();
-                                }
+                                           @Override
+                                           public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                                               final String boardID = dataSnapshot.getKey();
+                                               adapter.remove(boardID);
+                                               adapter.notifyDataSetChanged();
+                                           }
 
-                            }
-                        }
+                                           @Override
+                                           public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                                               adapter.notifyDataSetChanged();
+                                           }
 
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                        }
-                    });
-
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
+                                           @Override
+                                           public void onCancelled(@NonNull DatabaseError databaseError) {
+                                               Log.e("취소 에러 확인 : ", databaseError.getMessage());
+                                           }
+                                       }
+        );
     }
+
 }
