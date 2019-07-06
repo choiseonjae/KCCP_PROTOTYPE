cf96e45a13fdbf084792d4f76ac862cdce6b9d71
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2019-07-06 23:51:47 +0900

    commit Test 한번만 하겠슴다..

--- app/src/main/java/com/example/androideatit/MainActivity.java
+++ app/src/main/java/com/example/androideatit/MainActivity.java
@@ -17,7 +17,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+//commit Test
     private BackPressCloseHandler backPressCloseHandler;
     private String userID;
     TextView txtSlogan;
